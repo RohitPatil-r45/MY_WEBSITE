f284d608ef454e7157610965e7336808d48aedc6
Rohit Patil <superfissionr45@gmail.com>
2021-08-22 20:13:20 +0530

    error page style change

--- website/src/Error.js
+++ website/src/Error.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import errorPage from "./images/error_page.png";
 const Error = () => {
   return (
     <section className="pos">
       <div className="error-container">
-        <h1>Error Page</h1>
+        <img src={errorPage} alt="Error" className="error-img" />
+
         <Link to="/">
           <button className="back-btn">Back to Home</button>
         </Link>
