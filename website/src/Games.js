19366d38cdb3edc3360e21660060bc436534cc6e
Rohit Patil <superfissionr45@gmail.com>
2021-08-23 11:49:21 +0530

    gyro changes

--- website/src/Games.js
+++ website/src/Games.js
@@ -1,17 +1,12 @@
 import React, { useState } from "react";
 import Helmet from "react-helmet";
 import Tilt from "react-parallax-tilt";
-// import { games } from "./data";
+
 import { GiWildfires, GiGamepad } from "react-icons/gi";
 import { BsToggleOff, BsToggleOn } from "react-icons/bs";
-// import clash from "./CR.jpg";
-// import bgmi from "./BGMI.png";
+
 const Games = () => {
-  // const [rotateContent, setRotateContent] = React.useState(false);
-  // const rotate = () => {
-  //   setRotateContent(!rotateContent);
-  // };
-  const [toggle, setToggle] = useState("false");
+  const [toggle, setToggle] = useState(false);
   return (
     <section className="pos">
       <Helmet>
@@ -43,18 +38,6 @@ const Games = () => {
           </div>
         )}
       </div>
-      {/* {games.map((game) => {
-          const { id, name, url, game_name, game_id } = game;
-          return (
-            <div key={id} className="game-box">
-              <img src={clash} height="100px" width="100px" alt={game_name} />
-              <h1>{game_name}</h1>
-              <h3>Game Name : {name}</h3>
-              <h2>Game Id : {game_id}</h2>
-            </div>
-          );
-        })} */}
-
       <div className="games-container">
         <Tilt gyroscope={toggle}>
           <div className="game-box">
